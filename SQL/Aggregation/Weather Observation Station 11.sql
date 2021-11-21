76e598ff779d9a99b69749ccdb31188baebae213
nikoder28 <94730943+nikoder28@users.noreply.github.com>
2021-11-21 14:48:20 +0530

    Create Weather Observation Station 11.sql

---
+++ SQL/Aggregation/Weather Observation Station 11.sql
@@ -0,0 +1,5 @@
+select distinct city
+From station
+where
+( not (city like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u') 
+or not (city like 'A%' or city like 'E%' or city like 'I%' or city like 'O%' or city like 'U%'));
